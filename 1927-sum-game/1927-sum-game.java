8c2e62bd370ecc71ac0db3fc2528012fe1759d04
martand0007 <martand0007@gmail.com>
2026-08-23 17:37:54 +0530

    Time: 6 ms (97.08%), Space: 46.7 MB (91.24%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        int diff = leftSum - rightSum;
+        int compensation = (rightQ - leftQ) * 9 / 2;
+
+        return diff != compensation;
+    }
+}
